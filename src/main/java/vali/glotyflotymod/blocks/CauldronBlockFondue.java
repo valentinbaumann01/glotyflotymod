a6ed443d39467ab6281fbe37b784e742ca39b05b
vba116912 <vba116912@iet-gibb.ch>
2021-04-05 10:48:03 +0200

    override cauldron onBloickActivete to register fondue like water

--- src/main/java/vali/glotyflotymod/blocks/CauldronBlockFondue.java
+++ src/main/java/vali/glotyflotymod/blocks/CauldronBlockFondue.java
@@ -1,8 +1,19 @@
 package vali.glotyflotymod.blocks;
 
+import net.minecraft.block.BlockState;
 import net.minecraft.block.CauldronBlock;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Hand;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.world.World;
+import net.minecraftforge.registries.ForgeRegistries;
+import vali.glotyflotymod.lists.ItemList;
 
 public class CauldronBlockFondue extends CauldronBlock{
 
@@ -11,6 +22,27 @@ public class CauldronBlockFondue extends CauldronBlock{
 				.create(Material.GOURD)
 				);
 		this.setRegistryName(res);
+	}
+	
+	@Override
+	public ActionResultType onBlockActivated(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockRayTraceResult blockRayTraceResult) {
+		Item fonduebucket = ItemList.fondue_bucket;
+		Item waterbucket = ForgeRegistries.ITEMS.getValue(new ResourceLocation("water_bucket"));
+		
+		//replace to water from fondue on fill
+		if(ItemStack.areItemsEqual(player.getHeldItem(hand), new ItemStack(fonduebucket))) {
+			player.setHeldItem(Hand.MAIN_HAND, new ItemStack(waterbucket));
+		}
+		
+		//main bucket-super-logic
+		ActionResultType res = super.onBlockActivated(state, world, pos, player, hand, blockRayTraceResult);
+		
+		//replace to fondue from water on drain
+		if(ItemStack.areItemsEqual(player.getHeldItem(hand), new ItemStack(waterbucket))) {
+			player.setHeldItem(Hand.MAIN_HAND, new ItemStack(fonduebucket));
+		}
+		
+		return res;
 	}
 
 }
